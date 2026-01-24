9e69d0797e72fcfaf19a727e303d1e82305482a9
Andrii Shandryk(Invop) <shandrykjj512@gmail.com>
2026-01-24 22:19:02 +0200

    simplify msgs

--- src/SagaFlow/Messages/ISagaCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SagaFlow.Messages;
-
-/// <summary>
-///     Interface for saga commands.
-///     Commands represent actions that should be executed as part of a saga step.
-///     They are typically sent to a specific service to perform an operation.
-/// </summary>
-public interface ISagaCommand : ISagaMessage;
---
+++ src/SagaFlow/Messages/ISagaMessageHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace SagaFlow.Messages;
+
+public interface ISagaMessageHandler<in TMessage> where TMessage : ISagaMessage
+{
+    ValueTask HandleAsync(
+        ISagaMessageContext<TMessage> context,
+        CancellationToken cancellationToken = default);
+}
